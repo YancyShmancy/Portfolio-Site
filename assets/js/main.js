6e21b7ae3551b1ad59020501f46804c2f2975731
Zoey Yancy <yancyshmancy@gmail.com>
2016-03-16 03:07:14 -0400

    Finished site and added projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+jQuery(document).ready(function($) {  
+
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+    $(window).load(function(){
+        $('#preloader').delay(3000).fadeOut('slow',function(){$(this).remove();});
+    });
+});
+
 var $door = document.querySelector('.door');
 var $rotAssembly = document.querySelector('.rotational-assembly');
 var $assembly = document.querySelector('.assembly');
@@ -40,6 +48,9 @@ var moveForward = function() {
     if ( posZ >= 2850 && (!$($door).hasClass('clicked')) ) {
         
         return false;
+    } else if ( posZ >= 4700 && (-90 < rotationNumber < 90)) {
+        
+        return false;
     } else {
         
         TweenLite.to($rotAssembly, 0.1, {
@@ -67,10 +78,7 @@ var rotateRight = function() {
 
 var moveBack = function() {
     
-    if ( posZ >= 4700 && (-270 < rotationNumber < -90) ) {
-        
-        return false;
-    } else if ( posZ <= 0) {
+    if ( posZ <= 0) {
         
         return false;
     } else {
@@ -157,7 +165,24 @@ $(document).keydown(function(e) {
             }
         break;
         case 87: // W
-            upArrowClicked();
+            if ( (rotationNumber > -90 && rotationNumber < 90) || rotationNumber < -270 || rotationNumber > 270 ) {
+                upArrowClicked();
+            } else if ( (rotationNumber < -90 && rotationNumber > -270) || (rotationNumber > 90 && rotationNumber < 270) ) {
+                backArrowClicked();
+            }
+        break;
+        case 65: // A
+            leftArrowClicked();
+        break;
+        case 83: // S
+            if ( (rotationNumber > -90 && rotationNumber < 90) || rotationNumber < -270 || rotationNumber > 270) {
+                backArrowClicked();
+            } else if ( (rotationNumber < -90 && rotationNumber > -270) || (rotationNumber > 90 && rotationNumber < 270) ) {
+                upArrowClicked();
+            }
+        break;
+        case 68: // D
+            rightArrowClicked();
         break;
 
         default: return; // exit this handler for other keys
@@ -167,10 +192,10 @@ $(document).keydown(function(e) {
 
 $('.door').on('click', function() {
     $(this).addClass('clicked');
-//    $($rotAssembly).append('<div class="light-face big-floor"></div><div class="dark-face big-ceiling"></div><div class="dark-face mini-left-wall"></div><div class="dark-face mini-right-wall"></div><div class="light-face big-far-wall"><div class="lights"><div class="light"></div><div class="light"></div><div class="light"></div></div><h1 class="shadow">YancyShmancy Portfolio</h1><h2 class="shadow">Developer / Designer</h2><div class="projects-container"><div class="top-row"><div class="project-square" id="safe-house"><div class="project-square--front"></div></div><div class="project-square" id="yosemite"><div class="project-square--front"></div></div><div class="project-square" id="advanced-js"><div class="project-square--front"></div></div></div><div class="bottom-row"><div class="project-square" id="game-dev"><div class="project-square--front"></div></div><div class="project-square" id="hot-sauce"><div class="project-square--front"></div></div></div></div></div><div class="light-face big-left-side"></div><div class="light-face big-right-side"></div>')
+    $($rotAssembly).append('<div class="light-face big-floor texture"></div><div class="dark-face big-ceiling texture"></div><div class="dark-face mini-left-wall texture"></div><div class="dark-face mini-right-wall texture"></div><div class="light-face big-far-wall texture"><div class="lights"><div class="light"></div><div class="light"></div><div class="light"></div></div><h1 class="shadow">My Portfolio</h1><h2 class="shadow">Developer / Designer</h2><div class="projects-container"><div class="top-row"><div class="project-square" id="safe-house"><button role="button" class="project-square--front" data-toggle="modal" data-target="#sh-modal"></button></div><div class="project-square" id="yosemite"><button role="button" data-toggle="modal" data-target="#yosemite-modal" class="project-square--front"></button></div><div class="project-square" id="advanced-js"><button role="button" data-toggle="modal" data-target="#adv-js-modal" class="project-square--front"></button></div></div><div class="bottom-row"><div class="project-square" id="game-dev"><button role="button" data-toggle="modal" data-target="#game-dev-modal" class="project-square--front"></button></div><div class="project-square" id="hot-sauce"><button role="button" data-target="#hot-sauce-modal" data-toggle="modal" class="project-square--front"></button></div></div></div></div><div class="light-face big-left-side texture"><div class="aboutMe-container"><h1>About Me</h1><div class="col-xs-6 col-xs-offset-1"><p>Hey, there. I&#39;m Zoey, an interactive designer and developer. I love experimenting with coding, and I enjoy pushing my limits as a developer. Welcome to my first person 3D experience, created with HTML and CSS transforms. Check out my projects to your right, or send me a message with the form behind you.</p></div><div class="col-xs-5"><img src="./assets/img/IMG_0740.JPG" alt=""></div></div></div><div class="light-face big-right-side texture"><h1 class="contact">Contact Me</h1><form class="contactMe"><fieldset class="form-group"><label for="exampleInputEmail1">Email Address</label><input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" required="true"></fieldset><fieldset class="form-group"><label for="name">Your Name</label><input type="name" class="form-control" id="name" placeholder="Enter Your Name"></fieldset><fieldset class="form-group"><label for="exampleTextarea">Your Message</label><textarea class="form-control" id="exampleTextarea" rows="3" required="true"></textarea></fieldset><button type="submit" class="btn btn-primary btn-lg">Submit</button></form></div>');
 });
 
 $($introBackground).on('click', function() {
     
-    $(this).parent().addClass('hidden');
+    $(this).parent().fadeOut('slow',function(){$(this).addClass('hidden');});
 })
